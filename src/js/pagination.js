fd9196f613d4372481b521f799f5dbddc4182794
MishaKhmara <1927mishakhmara03@gmail.com>
2021-03-26 09:18:19 +0200

    pagination-8

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -2,6 +2,7 @@
 import paginate from 'handlebars-paginate';
 import * as Handlebars from 'handlebars/runtime';
 import paginationTpl from '../templates/pagination.hbs';
+import paginationOnePage from '../templates/paginationOnePage.hbs';
 import content from './content';
 
 Handlebars.registerHelper('paginate', paginate);
@@ -11,26 +12,30 @@ export default {
     pagDiv: null,
     incrementBtn: null,
     decrementBtn: null,
-    repos: null,
+    reposLink: null,
     page: 1,
 
     linkParent(selector) {
         this.parentNode = document.querySelector(selector);
     },
     linkRefs() {
-        this.repos = document.querySelector('.repos-pagination__link');
         this.incrementBtn = document.querySelector('#inc');
         this.decrementBtn = document.querySelector('#dec');
+        this.reposLink = document.querySelector('.pag-btn');
         this.paginationRef = document.querySelector('.pagination');
     },
 
     bindEvents() {
         this.incrementBtn.addEventListener('click', this.increment.bind(this));
         this.decrementBtn.addEventListener('click', this.decrement.bind(this));
+        this.reposLink.addEventListener(
+            'click',
+            this.onReposeLincClick.bind(this),
+        );
     },
 
     render() {
-        console.log(content.page, content.pageCount);
+        // console.log(content.page, content.pageCount);
         this.pagMarkup();
         this.linkRefs();
         this.bindEvents();
@@ -39,13 +44,16 @@ export default {
     pagMarkup() {
         let page = content.page;
         let pageCount = content.pageCount;
-
-        this.parentNode.innerHTML = paginationTpl({
-            pagination: {
-                page,
-                pageCount,
-            },
-        });
+        if (pageCount < 2) {
+            this.parentNode.innerHTML = '';
+        } else {
+            this.parentNode.innerHTML = paginationTpl({
+                pagination: {
+                    page,
+                    pageCount,
+                },
+            });
+        }
     },
 
     increment() {
@@ -60,12 +68,19 @@ export default {
     },
 
     decrement() {
-        // console.log(content.page);
         if (this.page <= 1) {
             return;
         }
         this.page -= 1;
         content.page = this.page;
         content.render();
+    },
+    onReposeLincClick(e) {
+        e.preventDefault();
+        console.log(e);
+        // this.page = e.target.dataset;
+        // content.page = this.page;
+
+        // content.render();
     },
 };
